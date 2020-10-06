8eea76e3cba30b6374d157cf883ecbbcf53fc065
i-jey <ilakkiyan.jeyakumar@gmail.com>
2020-10-06 13:14:36 -0400

    Refactored num_bottles calculator to use conversions util, added a couple test cases

--- src/calculators/num_bottles.rs
+++ src/calculators/num_bottles.rs
@@ -1,3 +1,4 @@
+use crate::utils::conversions::VolumeBuilder; // Converts string input to unit measurements
 /// A struct used to print how many bottles are needed to contain a given volume (in mL or L)
 pub struct NumBottles;
 
@@ -19,38 +20,67 @@ impl NumBottles {
             ("5 liter mini keg".to_string(), 5000.0),
         ]
     }
-    /// Prints out the quantity of bottles needed to contain a volume given in mL
+    /// Prints out the quantity of bottles needed to store a given volume
     ///
     /// # Arguments
-    /// * 'volume' - A f32 in milliliters of the volume to hold
+    /// * 'volume' - A string of the volume to hold, with units appended e.g "10mL", "5.4gal", "3.99L" etc.
     ///
-    pub fn bottles_milliliters(&self, volume: f32) {
-        println!("VOLUME GIVEN: {} mL\n=============", volume);
-        for bottle in self.bottles() {
-            let num_bottles: f32 = volume / bottle.1;
-            let output = format!(
-                "Type: {0: <20} | Number: {1: <5} |",
-                bottle.0,
-                num_bottles.ceil()
-            );
-            println!("{}", output);
+    pub fn calculate_num_bottles(&self, volume: String) -> Vec<(String, i32)> {
+        let bottle_types = self.bottles();
+        let mut bottle_counter: Vec<(String, i32)> = Vec::with_capacity(bottle_types.len());
+        let volume = VolumeBuilder::from_str(volume).as_milliliters() as f32;
+
+        for bottle in bottle_types {
+            let num_bottles: i32 = ((volume) / bottle.1).ceil() as i32;
+            bottle_counter.push((bottle.0, num_bottles));
         }
+        bottle_counter
     }
-    /// Prints out the quantity of bottles needed to contain a volume given in L
-    ///
-    /// # Arguments
-    /// * 'volume' - A f32 in liters of the volume to hold
-    ///
-    pub fn bottles_liters(&self, volume: f32) {
-        println!("\nVOLUME GIVEN: {} L\n=============", volume);
-        for bottle in self.bottles() {
-            let num_bottles: f32 = (volume * 1000.0) / bottle.1;
-            let output = format!(
-                "Type: {0: <20} | Number: {1: <5} |",
-                bottle.0,
-                num_bottles.ceil()
-            );
-            println!("{}", output);
-        }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_milliliters() {
+        let expected = vec![
+            ("330ml bottle".to_string(), 1),
+            ("Twelve ounce bottle".to_string(), 1),
+            ("Groish bottle".to_string(), 1),
+            ("Half liter bottle".to_string(), 1),
+            ("Pint".to_string(), 1),
+            ("Graft bottle".to_string(), 1),
+            ("Wine bottle".to_string(), 1),
+            ("Grenade jug".to_string(), 1),
+            ("Growler jug".to_string(), 1),
+            ("Gallon jug".to_string(), 1),
+            ("5 liter mini keg".to_string(), 1),
+        ];
+        assert_eq!(
+            expected,
+            NumBottles.calculate_num_bottles("330ml".to_string())
+        );
+    }
+
+    #[test]
+    fn test_liters() {
+        let expected = vec![
+            ("330ml bottle".to_string(), 1000),
+            ("Twelve ounce bottle".to_string(), 930),
+            ("Groish bottle".to_string(), 734),
+            ("Half liter bottle".to_string(), 660),
+            ("Pint".to_string(), 581),
+            ("Graft bottle".to_string(), 508),
+            ("Wine bottle".to_string(), 440),
+            ("Grenade jug".to_string(), 349),
+            ("Growler jug".to_string(), 175),
+            ("Gallon jug".to_string(), 88),
+            ("5 liter mini keg".to_string(), 66),
+        ];
+        assert_eq!(
+            expected,
+            NumBottles.calculate_num_bottles("330l".to_string())
+        );
     }
 }
