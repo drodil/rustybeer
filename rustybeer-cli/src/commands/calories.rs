8e8fa32724c51f7f70882319ff866a928f091a93
rogercyyu <roger.yu27@gmail.com>
2020-10-27 14:16:04 -0400

    address PR comments

--- rustybeer-cli/src/commands/calories.rs
+++ rustybeer-cli/src/commands/calories.rs
@@ -1,9 +1,10 @@
 use clap::{value_t, App, Arg, ArgMatches};
-pub use rustybeer::calculators::calorie_counter::{
+use rustybeer::calculators::calorie_counter::{
     calculate_alcohol_calories, calculate_carbs_calories, calculate_total_calories,
 };
-pub use rustybeer_util::abv_calories::{ABVCalories, Criteria, ABV_CALORIES};
-pub use rustybeer_util::conversions::{MassBuilder, VolumeBuilder};
+use rustybeer::calculators::num_bottles::bottles;
+use rustybeer_util::abv_calories::{Criteria, ABV_CALORIES};
+use rustybeer_util::conversions::{MassBuilder, VolumeBuilder};
 
 pub fn add_subcommand<'a, 'b>() -> App<'a, 'b> {
     App::new("calories")
@@ -35,9 +36,8 @@ pub fn add_subcommand<'a, 'b>() -> App<'a, 'b> {
                  .short("v")
                  .long("volume")
                  .value_name("vol")
-                 .help("volume as string ('e.g 10m, 4gal')")
+                 .help("Volume as a string ('e.g 10mL, 4gal')")
                  .required(false)
-                 .default_value("100ml")
                  .takes_value(true))
 }
 
@@ -49,42 +49,105 @@ fn matches_to_criteria<'a>(matches: &ArgMatches<'a>) -> Criteria {
 
 pub fn do_matches<'a>(matches: &ArgMatches<'a>) {
     if let Some(matches) = matches.subcommand_matches("calories") {
-        if matches.is_present("fg") && matches.is_present("og") {
+        let conversion = MassBuilder::new("12oz").unwrap().as_grams() as f32;
+        if matches.is_present("fg") && matches.is_present("og") && !matches.is_present("abv") {
             let fg = value_t!(matches, "fg", f32).unwrap_or_else(|e| e.exit());
             let og = value_t!(matches, "og", f32).unwrap_or_else(|e| e.exit());
-            let cv = value_t!(matches, "volume", String).unwrap_or_else(|e| e.exit());
-            let conversion = MassBuilder::from_str("12oz").unwrap().as_grams() as f32;
-            let vol = VolumeBuilder::from_str(&cv).unwrap().as_milliliters() as f32;
-            let ac = calculate_alcohol_calories(og, fg) / conversion * vol;
-            let cc = calculate_carbs_calories(og, fg) / conversion * vol;
-            let tc = calculate_total_calories(og, fg) / conversion * vol;
-            println!("{:<8} {:>8} {:>8}", "", "kcal", "kJ");
-            println!("{:<8} {:>8.0} {:>8.0}", "Alcohol:", ac, ac * 4.184);
-            println!("{:<8} {:>8.0} {:>8.0}", "Carbs:", cc, cc * 4.184);
-            println!("{:<8} {:>8.0} {:>8.0}", "Total:", tc, tc * 4.184);
-            println!("Per: {:.0} ml", vol);
-        } else if matches.is_present("abv") {
-            let cv = value_t!(matches, "volume", String).unwrap_or_else(|e| e.exit());
-            let criteria = matches_to_criteria(matches);
-
-            for abv in ABV_CALORIES.iter() {
-                if criteria.matches(abv) {
-                    let conversion = MassBuilder::from_str("12oz").unwrap().as_grams() as f32;
-                    let vol = VolumeBuilder::from_str(&cv).unwrap().as_milliliters() as f32;
-                    let lc = abv.calories_low / conversion * vol;
-                    let hc = abv.calories_high / conversion * vol;
-                    println!("Calories: {:.0} to {:.0} kcal", lc, hc);
-                    println!("          {:.0} to {:.0} kJ", lc * 4.184, hc * 4.184);
-                    println!("Per: {:.0} ml", vol);
-                    return;
+            if matches.is_present("volume") {
+                let vol = value_t!(matches, "volume", String).unwrap_or_else(|e| e.exit());
+                let volume = VolumeBuilder::new(&vol).unwrap().as_milliliters();
+                let ac = calculate_alcohol_calories(og, fg) / conversion * volume as f32;
+                let cc = calculate_carbs_calories(og, fg) / conversion * volume as f32;
+                let tc = calculate_total_calories(og, fg) / conversion * volume as f32;
+                println!("Estimated calories for: {} ml", volume);
+                println!("==============================");
+                println!("| {:<8} | {:<6} | {:<6} |", "", "kcal:", "kJ:");
+                println!("| {:<8} | {:>6.0} | {:>6.0} |", "Alcohol:", ac, ac * 4.184);
+                println!("| {:<8} | {:>6.0} | {:>6.0} |", "Carbs:", cc, cc * 4.184);
+                println!("| {:<8} | {:>6.0} | {:>6.0} |", "Total:", tc, tc * 4.184);
+                println!("==============================");
+            } else {
+                println!("Total estimated calories for:");
+                println!("==========================================================");
+                let bottles = calculate_calories_per_bottle(conversion, og, fg);
+                for bottle in bottles {
+                    let output = format!(
+                        "| Type: {: <20} | kcal: {: >6} | kJ: {: >6.0} |",
+                        bottle.0,
+                        bottle.1,
+                        bottle.1 * 4.184
+                    );
+                    println!("{}", output);
                 }
+                println!("==========================================================");
             }
-
-            println!("Could not find any ABV to calories matching criteria");
-            return;
+        } else if matches.is_present("abv")
+            && !matches.is_present("fg")
+            && !matches.is_present("og")
+        {
+            if matches.is_present("volume") {
+                let vol = value_t!(matches, "volume", String).unwrap_or_else(|e| e.exit());
+                let volume = VolumeBuilder::new(&vol).unwrap().as_milliliters() as f32;
+                let criteria = matches_to_criteria(matches);
+                for abv in ABV_CALORIES.iter() {
+                    if criteria.matches(abv) {
+                        let lc = abv.calories_low / conversion * volume;
+                        let hc = abv.calories_high / conversion * volume;
+                        println!("Total estimated calories range for: {} ml", volume);
+                        println!("=========================");
+                        println!("| {:>6.0} to {:<6.0} kcal |", lc, hc);
+                        println!("| {:>6.0} to {:<6.0} kJ   |", lc * 4.184, hc * 4.184);
+                        println!("=========================");
+                        return;
+                    }
+                }
+            } else {
+                let criteria = matches_to_criteria(matches);
+                for abv in ABV_CALORIES.iter() {
+                    if criteria.matches(abv) {
+                        println!("Total estimated calories range for:");
+                        println!("============================================================================");
+                        let bottles = get_list_of_volumes_from_bottles();
+                        for bottle in bottles {
+                            let lc = abv.calories_low / conversion * bottle.1;
+                            let hc = abv.calories_high / conversion * bottle.1;
+                            let output = format!(
+                                "| Type: {: <20} | kcal: {:>5.0} to {:<5.0} | kJ: {:>6.0} to {:<6.0} |",
+                                bottle.0, lc, hc, lc * 4.184, hc * 4.184
+                            );
+                            println!("{}", output);
+                        }
+                        println!("============================================================================");
+                        return;
+                    }
+                }
+            };
+            println!("Could not find any ABV to calories matching criteria (range: 0 to 22)");
         } else {
             println!("Either specify original gravity and final gravity or just abv!");
             println!("{}", matches.usage());
         }
     }
+}
+
+pub fn calculate_calories_per_bottle(conversion: f32, og: f32, fg: f32) -> Vec<(String, f32)> {
+    let bottle_types = bottles();
+    let mut bottle_counter: Vec<(String, f32)> = Vec::with_capacity(bottle_types.len());
+
+    for bottle in bottle_types {
+        let total_calories: f32 =
+            (calculate_total_calories(og, fg) / conversion * bottle.1 as f32).ceil();
+        bottle_counter.push((bottle.0, total_calories));
+    }
+    bottle_counter
+}
+
+pub fn get_list_of_volumes_from_bottles() -> Vec<(String, f32)> {
+    let bottle_types = bottles();
+    let mut bottle_counter: Vec<(String, f32)> = Vec::with_capacity(bottle_types.len());
+
+    for bottle in bottle_types {
+        bottle_counter.push((bottle.0, bottle.1 as f32));
+    }
+    bottle_counter
 }
