f688f0267a66c20a1517dcb446d3b778552c54a4
mlatief <m.latief@gmail.com>
2020-10-29 00:27:45 +0200

    Refactor calories and abv_abw calculators to use structopt

--- rustybeer-cli/src/commands/alcohol_volume_weight.rs
+++ rustybeer-cli/src/commands/alcohol_volume_weight.rs
@@ -1,94 +1,64 @@
-use clap::{value_t, App, Arg, ArgMatches};
 pub use rustybeer::calculators::alcohol_volume_weight::{
     calculate_abv_abw, calculate_abv_abw_density, calculate_abw_abv, calculate_abw_abv_density,
     calculate_alc_vol, calculate_alc_weight,
 };
-use rustybeer_util::conversions::VolumeBuilder;
+use rustybeer_util::{conversions::VolumeParser, measurements::Volume};
+use structopt::StructOpt;
 
-pub fn add_subcommand<'a, 'b>() -> App<'a, 'b> {
-    App::new("abv_abw")
-        .version("0.1")
-        .author("Joseph Russell (josephrussell123@gmail.com)")
-        .about("Calculates Alcohol by Weight (ABW) from Alcohol By Volue (ABV) and vice versa")
-        .arg(
-            Arg::with_name("percent")
-                .short("p")
-                .long("percent")
-                .value_name("PERCENT")
-                .help("'From' alcohol percentage")
-                .required(true)
-                .takes_value(true),
-        )
-        .arg(
-            Arg::with_name("volume")
-                .short("v")
-                .long("volume")
-                .value_name("VOLUME")
-                .help("Total beer volume")
-                .required(false)
-                .takes_value(true),
-        )
-        .arg(
-            Arg::with_name("density")
-                .short("d")
-                .long("density")
-                .value_name("DENSITY")
-                .help("Total density of beer in g/cm³")
-                .required(false)
-                .takes_value(true),
-        )
-        .arg(
-            Arg::with_name("reverse")
-                .short("r")
-                .long("reverse")
-                .value_name("REVERSE")
-                .help("Calculates ABW to ABV")
-                .required(false)
-                .takes_value(false),
-        )
+#[derive(Debug, StructOpt)]
+#[structopt(
+    name = "abv_abw",
+    author = "Joseph Russell (josephrussell123@gmail.com)"
+)]
+/// Calculates Alcohol by Weight (ABW) from Alcohol By Volue (ABV) and vice versa
+pub struct AbvAbwOptions {
+    #[structopt(short, long)]
+    /// 'From' alcohol percentage
+    percent: f64,
+
+    #[structopt(short, long, parse(try_from_str = VolumeParser::parse))]
+    /// Total beer volume
+    volume: Option<Volume>,
+
+    #[structopt(short, long, required_unless("fg"))]
+    /// Total density of beer in g/cm³
+    density: Option<f64>,
+
+    #[structopt(short, long)]
+    /// Calculates ABW to ABV
+    reverse: Option<bool>,
 }
 
-pub fn do_matches(matches: &ArgMatches) {
-    if let Some(matches) = matches.subcommand_matches("abv_abw") {
-        if matches.is_present("percent") {
-            let percent = value_t!(matches, "percent", f64).unwrap_or_else(|e| e.exit());
-            let end_percentage: f64;
+pub fn calculate_and_print(abv_abw: AbvAbwOptions) {
+    let end_percentage: f64;
+    // main ABV <-> ABW conversion
+    if Some(true) == abv_abw.reverse {
+        end_percentage = match abv_abw.density {
+            Some(density) => calculate_abw_abv_density(abv_abw.percent, density),
+            None => calculate_abw_abv(abv_abw.percent),
+        };
 
-            // main ABV <-> ABW conversion
-            if matches.is_present("reverse") {
-                if matches.is_present("density") {
-                    let density = value_t!(matches, "density", f64).unwrap_or_else(|e| e.exit());
-                    end_percentage = calculate_abw_abv_density(percent, density);
-                } else {
-                    end_percentage = calculate_abw_abv(percent);
-                }
-                println!("ABV: {:.3}%", end_percentage);
-            } else {
-                if matches.is_present("density") {
-                    let density = value_t!(matches, "density", f64).unwrap_or_else(|e| e.exit());
-                    end_percentage = calculate_abv_abw_density(percent, density);
-                } else {
-                    end_percentage = calculate_abv_abw(percent);
-                }
-                println!("ABW: {:.3}%", end_percentage);
-            }
+        println!("ABV: {:.3}%", end_percentage);
+    } else {
+        end_percentage = match abv_abw.density {
+            Some(density) => calculate_abv_abw_density(abv_abw.percent, density),
+            None => calculate_abv_abw(abv_abw.percent),
+        };
+        println!("ABW: {:.3}%", end_percentage);
+    }
 
-            // Quantity of alcohol
-            if matches.is_present("volume") {
-                let volume = value_t!(matches, "volume", String).unwrap_or_else(|e| e.exit());
-                let volume_ml = VolumeBuilder::new(&volume).unwrap().as_millilitres();
-                if matches.is_present("reverse") {
-                    println!(
-                        "Alcohol: {:.3} ml",
-                        calculate_alc_vol(volume_ml, end_percentage)
-                    );
-                } else {
-                    println!("Alcohol: {:.3} g", calculate_alc_weight(volume_ml, percent));
-                }
-            }
+    // Quantity of alcohol
+    if let Some(volume) = abv_abw.volume {
+        if Some(true) == abv_abw.reverse {
+            println!(
+                "Alcohol: {:.3} ml",
+                calculate_alc_vol(volume.as_millilitres(), end_percentage)
+            );
         } else {
-            println!("The alcohol percentage has not been specified.");
-            println!("{}", matches.usage());
+            println!(
+                "Alcohol: {:.3} g",
+                calculate_alc_weight(volume.as_millilitres(), abv_abw.percent)
+            );
         }
     }
 }
--- rustybeer-cli/src/main.rs
+++ rustybeer-cli/src/main.rs
@@ -7,8 +7,10 @@ mod commands;
 /// RustyBeer Calculators CLI
 pub enum RustyBeer {
     Abv(commands::abv::AbvOptions),
+    AbvAbw(commands::alcohol_volume_weight::AbvAbwOptions),
     BeerStyle(commands::beer_style::BeerStyleOptions),
     BoilOff(commands::boil_off::BoilOffOptions),
+    Calories(commands::calories::CaloriesOptions),
     Diluting(commands::diluting::DilutingOptions),
     NumBottles(commands::num_bottles::NumBottlesOptions),
     Priming(commands::priming::PrimingOptions),
@@ -19,8 +21,10 @@ fn main() -> Result<()> {
     let opt = RustyBeer::from_args_safe().with_context(|| "wrong arguments")?;
     match opt {
         RustyBeer::Abv(opts) => commands::abv::calculate_and_print(opts),
+        RustyBeer::AbvAbw(opts) => commands::alcohol_volume_weight::calculate_and_print(opts),
         RustyBeer::BeerStyle(opts) => commands::beer_style::calculate_and_print(opts),
         RustyBeer::BoilOff(opts) => commands::boil_off::calculate_and_print(opts),
+        RustyBeer::Calories(opts) => commands::calories::calculate_and_print(opts),
         RustyBeer::Diluting(opts) => commands::diluting::calculate_and_print(opts),
         RustyBeer::NumBottles(opts) => commands::num_bottles::calculate_and_print(opts),
         RustyBeer::Priming(opts) => commands::priming::calculate_and_print(opts),
